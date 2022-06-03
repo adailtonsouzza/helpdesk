56256dd0b5dc2f4f8571291e1c601633ed597b95
adailtonsouzza <adailtonsouza18@gmail.com>
2022-06-03 15:46:51 -0300

    ajustes ao atualizar senha

--- src/main/java/com/adailton/helpdesk/services/ClienteService.java
+++ src/main/java/com/adailton/helpdesk/services/ClienteService.java
@@ -50,8 +50,12 @@ public class ClienteService {
 	
 	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
 		objDTO.setId(id);
-		objDTO.setSenha(encoder.encode(objDTO.getSenha()));
 		Cliente oldObj = findById(id);
+
+		if(!objDTO.getSenha().equals(oldObj.getSenha())){
+			objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		}
+
 		validaPorCpfEEmail(objDTO);
 		oldObj = new Cliente(objDTO);
 		return repository.save(oldObj);
